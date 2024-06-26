abd56a93f9f86460db38a15e0834402130829baa
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2024-06-26 16:13:55 -0300

    fix: astro rendering markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
-export default defineConfig( {
+export default defineConfig({
   site: "https://yangricardo.github.io",
-  integrations: [
-    tailwind( {
-      applyBaseStyles: false,
-      nesting: true
-    } ),
-    icon()
-  ]
-} );
+  integrations: [tailwind({
+    applyBaseStyles: false,
+    nesting: true
+  }), icon(), mdx(), markdoc()]
+});
