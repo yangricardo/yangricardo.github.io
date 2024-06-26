9f50724f1aa606a13431040532f722dfdb15c87e
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2024-06-25 21:44:23 -0300

    config: tailwind setup updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()]
-});
+export default defineConfig( {
+  integrations: [ tailwind(
+    {
+      applyBaseStyles: false,
+      nesting: true,
+    }
+  ) ]
+} );
