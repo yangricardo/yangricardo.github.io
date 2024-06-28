960e5b29308b155df6123e95a7bc4e662c631aa4
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2024-06-28 18:06:51 -0300

    config: robots.txt and sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-
 import markdoc from "@astrojs/markdoc";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({
-  site: "https://yangricardo.github.io",
-  integrations: [tailwind({
+export default defineConfig( {
+  site: "https://yang.dev.br",
+  integrations: [ tailwind( {
     applyBaseStyles: false,
     nesting: true
-  }), icon(), mdx(), markdoc()]
-});
+  } ), icon(), mdx(), markdoc(), sitemap() ]
+} );
