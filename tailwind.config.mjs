9cf464eba68923136568c8e6fcdd9d94048deed7
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2024-06-28 00:37:45 -0300

    feat: home page improvements and typewriting animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,25 @@ export default {
         'serif': [ 'Trirong', 'serif' ],
         'mono': [ 'Fira Code', 'monospace' ],
       },
+      animation: {
+        type: 'type 2.7s ease-out .8s infinite alternate both',
+      },
+      keyframes: {
+        type: {
+          '0%': { width: '0ch' },
+          '5%, 10%': { width: '1ch' },
+          '15%, 20%': { width: '2ch' },
+          '25%, 30%': { width: '3ch' },
+          '35%, 40%': { width: '4ch' },
+          '45%, 50%': { width: '5ch' },
+          '55%, 60%': { width: '6ch' },
+          '65%, 70%': { width: '7ch' },
+          '75%, 80%': { width: '8ch' },
+          '85%, 90%': { width: '9ch' },
+          '95%': { width: '10ch' },
+        },
+      },
+
     },
   },
   plugins: [
